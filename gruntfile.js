3a5b400897fd413bc106adce6c429e6569964e1c
Ryan <ryan@burgettweb.net>
2014-10-19 22:52:05 -0400

    Added Grunt Watch

--- gruntfile.js
+++ gruntfile.js
@@ -19,12 +19,22 @@ module.exports = function(grunt) {
 				src: 'js/build/production.js',
 				dest: 'js/build/production.min.js'
 			}
+		},
+		watch: {
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			}
 		}
 	});
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
 	grunt.registerTask('default', ['concat', 'uglify']);
