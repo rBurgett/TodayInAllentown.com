a6bf22952104f9af8b56a1770332898e6c318222
Ryan <ryan@burgettweb.net>
2014-10-19 22:40:41 -0400

    Added Grunt taskrunner

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+	// 1. All configuration goes here 
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			dist: {
+				src: [
+					'js/jquery-1.11.1.min.js',
+					'js/handlebars.min.js',
+					'js/main.js'
+			],
+			dest: 'js/build/production.js'
+			}
+		},
+		uglify: {
+			build: {
+				src: 'js/build/production.js',
+				dest: 'js/build/production.min.js'
+			}
+		}
+	});
+
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('default', ['concat', 'uglify']);
+
+};
