ec2c67cc2b9cc49d20f9d681df2f2aa22f3c460a
Ryan <ryan@burgettweb.net>
2014-10-20 01:11:24 -0400

    Began building TIA class

--- js/build/production.js
+++ js/build/production.js
@@ -34,6 +34,33 @@ this.Handlebars=function(){var a=function(){"use strict";function a(a){this.stri
 /* jshint undef: true, unused: true, strict: true, browser: true, devel: true */
 /* global $ */
 
+var TIA = {
+	_feedList : [],
+	create : function (feeds) {
+		'use strict';
+		return Object.create(TIA).init(feeds);
+	},
+	init : function(feeds) {
+		'use strict';
+		this._feedList = feeds || [];
+		return this;
+	},
+	addFeed : function (feed) {
+		'use strict';
+		this._feedList.push(feed);
+	},
+	feedList : function () {
+		'use strict';
+		return this._feedList;
+	}
+};
+var tIA = TIA.create([
+		'http://www.wfmz.com/news/news-regional-lehighvalley/132502?format=rss_2.0&view=feed',
+		'http://www.mcall.com/news/local/rss2.0.xml',
+		'http://blog.lehighvalleylive.com/lvnews_impact/atom.xml'
+	]);
+console.log(tIA.feedList());
+
 $(document).ready(function () {
 	'use strict';
 	$('#jsnote').hide();
